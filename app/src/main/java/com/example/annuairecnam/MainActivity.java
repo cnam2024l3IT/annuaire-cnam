d549f8a5117b64076ba258cb7d8faaf41cb6a154
christopher TUHITI <christopher@ios.pf>
2024-06-13 21:55:20 -1000

    retrait test main

--- app/src/main/java/com/example/annuairecnam/MainActivity.java
+++ app/src/main/java/com/example/annuairecnam/MainActivity.java
@@ -19,9 +19,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DbManager dbManager;
-    private Context context;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,37 +29,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        context = this;
-        dbManager = new DbManager(context);
-
-        dbManager.open();
-
-//        Classe classe = new Classe("L3 informatique", "2023-2024");
-//        dbManager.insertClasse(classe);
-
-//        ArrayList<Eleve> eleves = new ArrayList<>();
-//        eleves.add(new Eleve("DOE", "John", "2004-06-13", "djohn@auditeur.cnam"));
-//        eleves.add(new Eleve("IAM", "Groot", "2004-06-13", "igroot@auditeur.cnam"));
-//        eleves.add(new Eleve("BRUH", "Lee", "2004-06-13", "blee@auditeur.cnam"));
-//        eleves.forEach(e -> dbManager.insertEleve(e));
-
-//        Matiere matiere = new Matiere("Mobile", "Développement d'application mobile", "RIVET Vaki");
-//        dbManager.insertMatiere(matiere);
-
-        ArrayList<Classe> classesList = dbManager.getAllClasses();
-        classesList.forEach(c -> Log.d("classes list", "onCreate: " + c.get_id() + " - " + c.getIntitule()));
-
-        ArrayList<Eleve> elevesList = dbManager.getAllEleves();
-        elevesList.forEach(e -> Log.d("eleves list", "onCreate: " + e.get_id() + " - " + e.getNom() + " " + e.getPrenom()));
-
-        ArrayList<Matiere> matieresList = dbManager.getAllMatieres();
-        matieresList.forEach(m -> Log.d("matieres list", "onCreate: " + m.get_id() + " - " + m.getIntitule()));
-    }
-
-    @Override
-    protected void onDestroy() {
-        dbManager.close();
-        super.onDestroy();
     }
 
 }
