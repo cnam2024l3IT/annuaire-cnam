3f426542bf62c211ac853592bbb68b163c025634
christopher TUHITI <christopher@ios.pf>
2024-06-24 18:18:58 -1000

    update layout edit eleve form

--- app/src/main/java/com/example/annuairecnam/activities/eleves/EleveEditActivity.java
+++ app/src/main/java/com/example/annuairecnam/activities/eleves/EleveEditActivity.java
@@ -10,15 +10,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.annuairecnam.R;
+import com.example.annuairecnam.adapters.EleveClasseListAdapter;
 import com.example.annuairecnam.databases.DbManager;
+import com.example.annuairecnam.models.Eleve;
 
 public class EleveEditActivity extends AppCompatActivity {
     private Context context;
     private DbManager dbManager;
     private EditText nomCtrl, prenomCtrl, dateNaissanceCtrl, emailCtrl, telephoneCtrl;
+    private RecyclerView listRv;
     private Button validerBtn;
+    private Eleve eleve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +36,35 @@ public class EleveEditActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        context = this;
+
+        nomCtrl = findViewById(R.id.eef_nom_ctrl);
+        prenomCtrl = findViewById(R.id.eef_prenom_ctrl);
+        dateNaissanceCtrl = findViewById(R.id.eef_date_naissance_ctrl);
+        emailCtrl = findViewById(R.id.eef_email_ctrl);
+        telephoneCtrl = findViewById(R.id.eef_telephone_ctrl);
+        listRv = findViewById(R.id.eef_classes_rv);
+
+
+        validerBtn = findViewById(R.id.eef_valider_btn);
+
+        dbManager = new DbManager(context).open();
+        if(getIntent().hasExtra(String.valueOf(R.string.eleve_tag))) {
+            eleve = getIntent().getParcelableExtra(String.valueOf(R.string.eleve_tag));
+            nomCtrl.setText(eleve.getNom());
+            prenomCtrl.setText(eleve.getPrenom());
+            dateNaissanceCtrl.setText(eleve.getDateNaissance());
+            emailCtrl.setText(eleve.getEmail());
+            telephoneCtrl.setText(eleve.getTelephone());
+//            listRv.setAdapter(new EleveClasseListAdapter(context, eleve, dbManager.getClassesByEleveId(eleve.get_id())));
+//            listRv.setLayoutManager(new LinearLayoutManager(context, RecyclerView.VERTICAL, false));
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        dbManager.close();
+        super.onDestroy();
     }
 }
